ba547b0b8b51cce3f857bf1840346f37a90e6ba1
polty-rishit <2023ugee075@nitjsr.ac.in>
2025-07-29 07:17:15 +0530

    navbar

--- AIEI/src/components/Navbar.jsx
+++ AIEI/src/components/Navbar.jsx
@@ -58,9 +58,10 @@ const Navbar = () => {
                 </button>
                 {conferenceDropdown && (
                   <div className="absolute top-full left-0 mt-2 w-48 bg-white rounded-md shadow-lg py-2 z-10">
+                    <a href="General" className="block px-4 py-2 text-gray-800 hover:bg-gray-100 cursor-pointer">General Chair</a>
                     <a href="/committee" className="block px-4 py-2 text-gray-800 hover:bg-gray-100 cursor-pointer">Committee</a>
                     <a href="/venue" className="block px-4 py-2 text-gray-800 hover:bg-gray-100 cursor-pointer">Venue</a>
-                    <a href="General" className="block px-4 py-2 text-gray-800 hover:bg-gray-100 cursor-pointer">General Chair</a>
+                    
                   </div>
                 )}
               </div>
@@ -117,9 +118,10 @@ const Navbar = () => {
               </button>
               {conferenceDropdown && (
                 <div className="ml-4 mt-2 space-y-2">
+                  <a href="/general" className="block text-gray-800 hover:bg-white/30 rounded px-2 py-1 transition">General Chair</a>
                   <a href="/committee" className="block text-gray-800 hover:bg-white/30 rounded px-2 py-1 transition">Committee</a>
                   <a href="/venue" className="block text-gray-800 hover:bg-white/30 rounded px-2 py-1 transition">Venue</a>
-                  <a href="/general" className="block text-gray-800 hover:bg-white/30 rounded px-2 py-1 transition">General Chair</a>
+                  
                 </div>
               )}
             </div>
