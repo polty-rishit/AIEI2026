1a307e46b84c2f5f6b4561cd76b4b931977e0527
polty-rishit <2023ugee075@nitjsr.ac.in>
2025-08-02 16:29:13 +0530

    nit jsr change

--- AIEI/src/pages/Home.jsx
+++ AIEI/src/pages/Home.jsx
@@ -415,7 +415,7 @@ useEffect(() => {
       {/* About NIT Jamshedpur Section */}
       <section id="nitjsr" className="py-20 bg-gray-50">
         <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
-          <div className="grid grid-cols-1 lg:grid-cols-2 gap-12 items-center">
+          <div className="flex flex-col-reverse lg:flex-row gap-12 items-center">
             <div className="overflow-hidden rounded-lg shadow-lg">
               <img
                 src="./nitjsr.jpg"
