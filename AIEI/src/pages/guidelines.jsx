cbc5e6a206264e2f08a5ab29019e347c0786ca82
polty-rishit <2023ugee075@nitjsr.ac.in>
2025-08-07 23:22:04 +0530

    cmt error fix

--- AIEI/src/pages/guidelines.jsx
+++ AIEI/src/pages/guidelines.jsx
@@ -12,24 +12,7 @@ const Guidelines = () => {
         {/* Submission Website */}
         <section className="mb-10">
           <h2 className="text-2xl font-bold mb-2">Submission Website</h2>
-          <p className="mb-2">
-            Paper should be submitted using the Conference Management Tool{' '}
-             
-             <a
-              href="https://cmt3.research.microsoft.com/User/Login?ReturnUrl=%2F"
-              target="_blank"
-              rel="noopener noreferrer"
-              className="text-blue-600 "
-            >
-              Microsoft CMT
-            </a>.
-          </p>
-          <p className="mb-2">
-            The submission website will be open for submissions a week before the submission deadline of each round.
-          </p>
-          <p className="mb-2">
-            The Microsoft CMT service was used for managing the peer-reviewing process for this conference. This service was provided for free by Microsoft and they bore all expenses, including costs for Azure cloud services as well as for software development and support.
-          </p>
+           <p>The Microsoft CMT service was used for managing the peer-reviewing process for this conference. This service was provided for free by Microsoft and they bore all expenses, including costs for Azure cloud services as well as for software development and support.</p>
         </section>
 
         {/* Instructions for Research Papers */}
