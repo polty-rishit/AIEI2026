1ac1d0eac1102f7c726ea0dcf32ecbe789a017b3
polty-rishit <2023ugee075@nitjsr.ac.in>
2025-08-10 20:42:24 +0530

    cmt

--- AIEI/src/pages/CMT.jsx
+++ AIEI/src/pages/CMT.jsx
@@ -3,7 +3,10 @@ import React from 'react'
 const CMT = () => {
   return (
     <div>
-      <p>The Microsoft CMT service was used for managing the peer-reviewing process for this conference. This service was provided for free by Microsoft and they bore all expenses, including costs for Azure cloud services as well as for software development and support.</p>
+      <h1>CMT ACKNOWLEDGMENT</h1>
+  <p>
+    The Microsoft CMT service was used for managing the peer-reviewing process for this conference. This service was provided for free by Microsoft and they bore all expenses, including costs for Azure cloud services as well as for software development and support.
+  </p>
     </div>
   )
 }
