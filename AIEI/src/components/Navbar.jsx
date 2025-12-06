2b555f1ef917cb419b1804d6cb074ffca1991553
polty-rishit <2023ugee075@nitjsr.ac.in>
2025-12-06 20:05:00 +0530

    navbar change

--- AIEI/src/components/Navbar.jsx
+++ AIEI/src/components/Navbar.jsx
@@ -138,13 +138,36 @@ const handleRegistrationLeave = () => {
                   <div className="absolute top-full left-0 mt-2 w-48 bg-white rounded-md shadow-lg py-2 z-10">
                     <a href="/overview" className="block px-4 py-2 text-gray-800 hover:bg-gray-100 cursor-pointer">AIEI Overview</a>
                     <a href="/General" className="block px-4 py-2 text-gray-800 hover:bg-gray-100 cursor-pointer">General Chair</a>
-                    <a href="/patrons" className="block px-4 py-2 text-gray-800 hover:bg-gray-100 cursor-pointer">Patrons and Advisors</a>
+                    {/* <a href="/patrons" className="block px-4 py-2 text-gray-800 hover:bg-gray-100 cursor-pointer">Patrons and Advisors</a> */}
                     <a href="/committee" className="block px-4 py-2 text-gray-800 hover:bg-gray-100 cursor-pointer">Organizing Committee</a>
-                    <a href="/" className="block px-4 py-2 text-gray-800 hover:bg-gray-100 cursor-pointer">International Advisory Commitee (IAC)</a>
-                    <a href="/ieeeac" className="block px-4 py-2 text-gray-800 hover:bg-gray-100 cursor-pointer">IEEE Advisory Council</a>
-                    <a href="/ieeep" className="block px-4 py-2 text-gray-800 hover:bg-gray-100 cursor-pointer">IEEE Presidents(Sponsoring Societies)</a>
+                    {/* <a href="/" className="block px-4 py-2 text-gray-800 hover:bg-gray-100 cursor-pointer">International Advisory Commitee (IAC)</a> */}
+                    <a href="/ieeeac" className="block px-4 py-2 text-gray-800 hover:bg-gray-100 cursor-pointer">Advisory Committee</a>
+                    {/* <a href="/ieeep" className="block px-4 py-2 text-gray-800 hover:bg-gray-100 cursor-pointer">IEEE Presidents(Sponsoring Societies)</a> */}
                     <a href="https://www.ieee.org/" className="block px-4 py-2 text-gray-800 hover:bg-gray-100 cursor-pointer">About IEEE</a>
                     <a href="/contact" className="block px-4 py-2 text-gray-800 hover:bg-gray-100 cursor-pointer">Contact</a>
+                  </div>
+                )}
+              </div>
+
+              
+
+              {/* Authors Dropdown */}
+              <div className="relative" onMouseEnter={handleConferenceEnter} onMouseLeave={handleConferenceLeave}>
+                <button onClick={() => setConferenceDropdown(!conferenceDropdown)} className={`hover:text-blue-400 transition-colors cursor-pointer flex items-center ${isScrolled ? 'text-gray-900' : 'text-black'}`}>
+                  AUTHORS
+                  <svg className="w-4 h-4 ml-1" fill="none" stroke="currentColor" viewBox="0 0 24 24">
+                    <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M19 9l-7 7-7-7" />
+                  </svg>
+                </button>
+                {conferenceDropdown && (
+                  <div className="absolute top-full left-0 mt-2 w-48 bg-white rounded-md shadow-lg py-2 z-10">
+                    <a href="/" className="block px-4 py-2 text-gray-800 hover:bg-gray-100 cursor-pointer">Camera Ready Submission</a>
+                    <a href="/papers" className="block px-4 py-2 text-gray-800 hover:bg-gray-100 cursor-pointer">Call For Papers</a>
+                    
+                    <a href="/" className="block px-4 py-2 text-gray-800 hover:bg-gray-100 cursor-pointer">Author Information</a>
+                    
+              
+                    <a href="/cmt" className="block px-4 py-2 text-gray-800 hover:bg-gray-100 cursor-pointer">CMT Acknowledegemnt</a>
                   </div>
                 )}
               </div>
@@ -170,27 +193,6 @@ const handleRegistrationLeave = () => {
                     <a href="/" className="block px-4 py-2 text-gray-800 hover:bg-gray-100">Women in Antennas, Microwave and Space (WAMS) Luncheon Meeting</a>
                     <a href="/" className="block px-4 py-2 text-gray-800 hover:bg-gray-100">Start-Up Connect & Lounge</a>
                     <a href="/" className="block px-4 py-2 text-gray-800 hover:bg-gray-100">Chapter Chair Meeting</a> */}
-                  </div>
-                )}
-              </div>
-
-              {/* Authors Dropdown */}
-              <div className="relative" onMouseEnter={handleConferenceEnter} onMouseLeave={handleConferenceLeave}>
-                <button onClick={() => setConferenceDropdown(!conferenceDropdown)} className={`hover:text-blue-400 transition-colors cursor-pointer flex items-center ${isScrolled ? 'text-gray-900' : 'text-black'}`}>
-                  AUTHORS
-                  <svg className="w-4 h-4 ml-1" fill="none" stroke="currentColor" viewBox="0 0 24 24">
-                    <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M19 9l-7 7-7-7" />
-                  </svg>
-                </button>
-                {conferenceDropdown && (
-                  <div className="absolute top-full left-0 mt-2 w-48 bg-white rounded-md shadow-lg py-2 z-10">
-                    <a href="/" className="block px-4 py-2 text-gray-800 hover:bg-gray-100 cursor-pointer">Camera Ready Submission</a>
-                    <a href="/papers" className="block px-4 py-2 text-gray-800 hover:bg-gray-100 cursor-pointer">Call For Papers</a>
-                    
-                    <a href="/" className="block px-4 py-2 text-gray-800 hover:bg-gray-100 cursor-pointer">Author Information</a>
-                    
-              
-                    <a href="/cmt" className="block px-4 py-2 text-gray-800 hover:bg-gray-100 cursor-pointer">CMT Acknowledegemnt</a>
                   </div>
                 )}
               </div>
@@ -345,13 +347,35 @@ const handleRegistrationLeave = () => {
                 <div className="ml-4 mt-2 space-y-2">
                   <a href="/overview" className="block text-gray-800 hover:bg-white/30 rounded px-2 py-1 transition">AIEI Overview</a>
                   <a href="/general" className="block text-gray-800 hover:bg-white/30 rounded px-2 py-1 transition">General Chair</a>
-                  <a href="/patrons" className="block text-gray-800 hover:bg-white/30 rounded px-2 py-1 transition">Patrons and Advisors</a>
+                  {/* <a href="/patrons" className="block text-gray-800 hover:bg-white/30 rounded px-2 py-1 transition">Patrons and Advisors</a> */}
                   <a href="/committee" className="block text-gray-800 hover:bg-white/30 rounded px-2 py-1 transition">Organizing Committee</a>
-                  <a href="/" className="block text-gray-800 hover:bg-white/30 rounded px-2 py-1 transition">International Advisory Commitee (IAC)</a>
-                  <a href="/ieeeac" className="block text-gray-800 hover:bg-white/30 rounded px-2 py-1 transition">IEEE Advisory Council</a>
-                  <a href="/ieeep" className="block text-gray-800 hover:bg-white/30 rounded px-2 py-1 transition">IEEE Presidents(Sponsoring Societies)</a>
+                  {/* <a href="/" className="block text-gray-800 hover:bg-white/30 rounded px-2 py-1 transition">International Advisory Commitee (IAC)</a> */}
+                  <a href="/ieeeac" className="block text-gray-800 hover:bg-white/30 rounded px-2 py-1 transition">Advisory Committee</a>
+                  {/* <a href="/ieeep" className="block text-gray-800 hover:bg-white/30 rounded px-2 py-1 transition">IEEE Presidents(Sponsoring Societies)</a> */}
                   <a href="https://www.ieee.org/" className="block text-gray-800 hover:bg-white/30 rounded px-2 py-1 transition">About IEEE</a>
                   <a href="/contact" className="block text-gray-800 hover:bg-white/30 rounded px-2 py-1 transition">Contact</a>
+                </div>
+              )}
+            </div>
+
+            
+
+            {/* Mobile Authors */}
+            <div>
+              <button onClick={() => setConferenceDropdown(!conferenceDropdown)} className="w-full text-left text-gray-900 font-medium hover:bg-white/40 rounded-md px-3 py-2 transition flex items-center justify-between">
+                AUTHORS
+                <svg className="w-4 h-4 ml-2" fill="none" stroke="currentColor" viewBox="0 0 24 24">
+                  <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M19 9l-7 7-7-7" />
+                </svg>
+              </button>
+              {conferenceDropdown && (
+                <div className="ml-4 mt-2 space-y-2">
+                  <a href="/" className="block px-4 py-2 text-gray-800 hover:bg-gray-100 cursor-pointer">Camera Ready Submission</a>
+                  <a href="/papers" className="block px-4 py-2 text-gray-800 hover:bg-gray-100 cursor-pointer">Call For Papers</a>
+                  
+                  <a href="/" className="block px-4 py-2 text-gray-800 hover:bg-gray-100 cursor-pointer">Author Information</a>
+                  
+                  <a href="/cmt" className="block px-4 py-2 text-gray-800 hover:bg-gray-100 cursor-pointer">CMT Acknowledegemnt</a>
                 </div>
               )}
             </div>
@@ -377,26 +401,6 @@ const handleRegistrationLeave = () => {
                   <a href="/" className="block text-gray-800 hover:bg-white/30 rounded px-2 py-1 transition">Women in Antennas, Microwave and Space (WAMS) Luncheon Meeting</a>
                   <a href="/" className="block text-gray-800 hover:bg-white/30 rounded px-2 py-1 transition">Start-Up Connect & Lounge</a>
                   <a href="/" className="block text-gray-800 hover:bg-white/30 rounded px-2 py-1 transition">Chapter Chair Meeting</a> */}
-                </div>
-              )}
-            </div>
-
-            {/* Mobile Authors */}
-            <div>
-              <button onClick={() => setConferenceDropdown(!conferenceDropdown)} className="w-full text-left text-gray-900 font-medium hover:bg-white/40 rounded-md px-3 py-2 transition flex items-center justify-between">
-                AUTHORS
-                <svg className="w-4 h-4 ml-2" fill="none" stroke="currentColor" viewBox="0 0 24 24">
-                  <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M19 9l-7 7-7-7" />
-                </svg>
-              </button>
-              {conferenceDropdown && (
-                <div className="ml-4 mt-2 space-y-2">
-                  <a href="/" className="block px-4 py-2 text-gray-800 hover:bg-gray-100 cursor-pointer">Camera Ready Submission</a>
-                  <a href="/papers" className="block px-4 py-2 text-gray-800 hover:bg-gray-100 cursor-pointer">Call For Papers</a>
-                  
-                  <a href="/" className="block px-4 py-2 text-gray-800 hover:bg-gray-100 cursor-pointer">Author Information</a>
-                  
-                  <a href="/cmt" className="block px-4 py-2 text-gray-800 hover:bg-gray-100 cursor-pointer">CMT Acknowledegemnt</a>
                 </div>
               )}
             </div>
